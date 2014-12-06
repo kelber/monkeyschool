fb4e7d001e0eabb3b17a8e656f667f03648ee024
kelberstuchi <kelberstuchi@gmail.com>
2014-12-06 10:58:03 -0200

    initial less dragon

--- Gemfile
+++ Gemfile
@@ -36,7 +36,3 @@ gem 'masonry-rails'
 gem 'acts_as_votable', '~> 0.10.0'
 
 
-
-
-
-ruby "1.9.3"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
 
-	dragonfly_accessor :image, app: :assets
+	#dragonfly_accessor :image
+	# <%= image_tag post.image.thumb('400x200#').url , class: "thumbnail" if post.image_stored? %>
+	# <%= f.file_field :image %>
+	# <%= image_tag @post.image.thumb('400x200#').url , class: "thumbnail" if @post.image_stored? %>
+
 
 	acts_as_votable
 
